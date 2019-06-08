8eaeb165ee655ad069a66fa3c3deef0154ad938b
w3bwizart.be@gmail.com <w3bwizart.be@gmail.com>
2019-06-08 22:04:31 +0200

    added jest testing with css

--- src/W3bButton.test.js
+++ src/W3bButton.test.js
@@ -1,19 +1,8 @@
 import React from 'react';
-import W3bButton from './w3bButton.js/index.js';
-import renderer from 'react-test-renderer';
-import {shallow} from 'enzyme';
-
-it('renders an attributeless W3bButton without crashing', () => {
-  const component = renderer.create(<W3bButton />);
-  let tree = component.toJSON();
-  expect(component).toBeDefined();
-});
-
-it('renders a Disabled W3bButton', () => {
-  const component = renderer.create(<W3bButton disabled />);
-  let tree = component.toJSON();
-  expect(tree.props.disabled).toBe(true);
-  });
+import W3bButton from './w3bButton.js';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 
 it('renders a typed W3bButton', () => {
   const component = shallow(<W3bButton type='secondary' />);
@@ -22,10 +11,12 @@ it('renders a typed W3bButton', () => {
 
 it('renders a small sized W3bButton', () => {
   const component = shallow(<W3bButton size='small' />);
-  expect(component.props().size).toEqual('small');
+  console.log(component.props());
+  
+  expect(component.props().className).toEqual(' btn-small ');
 });
 
 it('renders a large sized W3bButton', () => {
   const component = shallow(<W3bButton size='large' />);
-  expect(component.props().size).toEqual('large');
+  expect(component.props().className).toEqual(' btn-large ');
 });
